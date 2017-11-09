945e6a4d21e0f19ec7dfe1b619a22ffacb81cc0e
yupenghe <yupeng.he.bioinfo@gmail.com>
2017-11-09 15:29:48 -0800

    update version number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='methylpy',
-    version='1.0.10',
+    version='1.0.11',
     author='Yupeng He',
     author_email='yupeng.he.bioinfo@gmail.com',
     packages=['methylpy'], 
